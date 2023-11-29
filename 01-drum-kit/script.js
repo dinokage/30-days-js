bebda28f585feeda7bdaa52647910181ebdf26b5
dinokage <yerradinesh01@gmail.com>
2023-11-29 21:32:30 +0530

    basic functionality added

--- 01-drum-kit/script.js
+++ 01-drum-kit/script.js
@@ -1,10 +1,17 @@
 const drumkit = document.getElementById('drumkit')
-const items = ['SNARE', 'KICK', 'HIHAT', 'OPENHAT', 'RIDE', 'TOM', 'CLAP', 'TINK', 'BOOM']
+const items = ['snare', 'kick', 'hihat', 'openhat', 'ride', 'tom', 'clap', 'tink', 'boom']
 console.log(drumkit)
 
-items.forEach(element => {
-    let x = document.createElement('span')
-    x.className = "sound"
-    x.textContent = element
-    drumkit.appendChild(x)
-});
+const playAudio = (elementID) => {
+    console.log(`playing ${elementID}`)
+    const audio = new Audio(`./sounds/${elementID}.wav`)
+    audio.play()
+}
+// items.forEach(element => {
+//     let x = document.createElement('span')  
+//     x.className = "sound"
+//     x.id = element
+//     x.textContent = element
+//     drumkit.appendChild(x)
+// });
+
